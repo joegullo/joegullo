0967c20d1c5e19f90bc1e64a5c62f9f90b655fac
Joe Gullo Digital Media <web@joegullo.com>
2025-02-24 16:37:11 -0500

    Update online-current-status.js

--- online-current-status.js
+++ online-current-status.js
@@ -1,30 +1,14 @@
-document.head.insertAdjacentHTML(
-    'beforeend',
-    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" />'); 
-
-document.head.insertAdjacentHTML(
-    'beforeend',
-    '<link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.23/dist/full.min.css" rel="stylesheet" type="text/css" />'); 
-
-document.head.insertAdjacentHTML(
-    'beforeend',
-    '<link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.23/dist/full.min.css" rel="stylesheet" type="text/css" />'); 
-
-document.head.insertAdjacentHTML(
-    'beforeend',
-    '<script src="https://cdn.tailwindcss.com"></script>'); 
-
 function checkOnlineStatus() {
 
         if (navigator.onLine) {
 
-            document.getElementById("online-status").innerHTML = '<p class="badge badge-outline"><i class="ri-circle-fill" style="color:green;font-size:10px;padding-right:3px;"></i> Online </p>'; 
+            document.getElementById("online-status").innerHTML = ' Online'; 
 
             // You can also change the icon here to a "live" icon
 
         } else {
 
-            document.getElementById("online-status").innerHTML = '<p class="badge badge-outline"> Offline </p>'; 
+            document.getElementById("online-status").innerHTML = 'Offline'; 
 
             // Display an "offline" icon
 
