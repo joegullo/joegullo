2eb3d32138440763d762528d94ad4c3e9d0ed8a7
Joe Gullo Digital Media <web@joegullo.com>
2025-02-26 13:48:41 -0500

    Update online-status.js

--- online-status.js
+++ online-status.js
@@ -3,11 +3,11 @@
 
             if (navigator.onLine) {
 
-                document.getElementById("online-status").innerHTML = `<div class="badge badge-outline"> Online <span class='online-status' style='color:green;'>●</span></div>`;
+                document.getElementById("online-status").innerHTML = `<div class="badge badge-outline"> Online <span class='online-status' style='color:green;padding-left:5px;'>●</span></div>`;
 
             } else {
 
-                document.getElementById("online-status").innerHTML = `Offline <span class='offline-status'>●</span>`;
+                document.getElementById("online-status").innerHTML = `<div class="badge badge-outline"> Offline <span class='offline-status' style='padding-left:5px;>●</span></div>`;
 
             }
 
