9abe368e2ce6e08610549b5867f63e6f367f361d
Joe Gullo Digital Media <web@joegullo.com>
2025-02-28 14:27:14 -0500

    Update current-online-status.js

--- current-online-status.js
+++ current-online-status.js
@@ -9,9 +9,6 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 
-
-Tawk_API = Tawk_API || {};
-
 Tawk_API.onStatusChange = function (status){
 
 if(status === 'online')
